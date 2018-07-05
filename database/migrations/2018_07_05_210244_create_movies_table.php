13c2e547966426fa9f31a561cd21598304c8c90f
topolovac <mtopolovac@pratura.io>
2018-07-05 23:27:51 +0200

    inital commit: movie,user,gengers models and migrations, dashboard controller

---
+++ database/migrations/2018_07_05_210244_create_movies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->unsignedInteger('genre_id');
+            $table->integer('year');
+            $table->integer('runtime');
+            $table->string('image');
+
+            $table->index('genre_id');
+            $table->foreign('genre_id')->references('id')->on('genres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
