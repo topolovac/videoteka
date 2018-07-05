47aeb89ea75cad0ebb8d479cdb32b69514375d48
topolovac <mtopolovac@pratura.io>
2018-07-06 00:00:02 +0200

    home controller, home view, dashboard controller and dashboard view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Dashboard extends Controller
+class DashboardController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class Dashboard extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController');
+
+Route::resource('dashboard', 'DashboardController');
