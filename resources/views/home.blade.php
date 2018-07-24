ae39d05b20a33fa6f46f84a07d45eaa01cae1098
topolovac <mtopolovac@pratura.io>
2018-07-24 21:59:21 +0200

    basic layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,69 +1,36 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+      <div class="navigation">
+        <a href="#" class="btn btn-primary">Dashboard</a>
+      </div>
+      <div class="content homepage">
+        <h1 id="main-title">Algebra Movies</h1>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <div class="container">
+          @php
+          $alphabet = array(
+            'A','B','C','D',
+            'E','F','G','H',
+            'I','J','K','L',
+            'M','N','O','P',
+            'Q','R','S','T',
+            'U','V','W','X',
+            'Y','Z');
+          @endphp
+          <ul>
+            @foreach ($alphabet as $key)
+              <a href="{{$key}}">{{$key}}</a>
+            @endforeach
+          </ul>
 
-            .homepage {
-              text-align: center;
-            }
-            .homepage a {
-              font-size: 25px;
-              padding: 8px;
-              text-decoration: none;
-              color: #636b6f;
-            }
-            #main-title {
-              font-size: 50px;
-              padding: 50px;
-            }
-          </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content homepage">
-              <h1 id="main-title">Algebra Movies</h1>
+          <ul>
 
-              <div class="container">
-                @php
-                $alphabet = array(
-                  'A','B','C','D',
-                  'E','F','G','H',
-                  'I','J','K','L',
-                  'M','N','O','P',
-                  'Q','R','S','T',
-                  'U','V','W','X',
-                  'Y','Z');
-                @endphp
-                <ul>
-                  @foreach ($alphabet as $key)
-                    <a href="{{$key}}">{{$key}}</a>
-                  @endforeach
-                </ul>
-
-                <ul>
-
-                </ul>
-              </div>
-
-            </div>
+          </ul>
         </div>
-    </body>
-</html>
+
+      </div>
+
+
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .homepage {
+              text-align: center;
+            }
+            .homepage a {
+              font-size: 25px;
+              padding: 8px;
+              text-decoration: none;
+              color: #636b6f;
+            }
+            #main-title {
+              font-size: 50px;
+              padding: 50px;
+            }
+
+            .navigation {
+              padding-top:15px;
+              padding-bottom:15px;
+            }
+          </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+          <div class="container">
+            @yield('content')
+          </div>
+
+
+        </div>
+    </body>
+</html>
