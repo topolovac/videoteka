3e3e6d3b86156a140202c5b1b0b0a2673d1443b7
topolovac <mtopolovac@pratura.io>
2018-07-25 11:27:49 +0200

    form styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,28 +7,36 @@
 @section('content')
   <h1>Dashboard</h1>
   <form>
-    <div class="form-group">
-      <label for="name">Movie name</label>
-      <input type="text" id="name" placeholder="Name" class="form-control">
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="name">Movie name</label>
+        <input type="text" id="name" placeholder="Name" class="form-control">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="name">Movie genre</label>
+        <input type="text" name="name" placeholder="Genre" class="form-control">
+      </div>
     </div>
-    <div class="form-group">
-      <label for="name">Movie genre</label>
-      <input type="text" name="name" placeholder="Genre" class="form-control">
+
+    <div class="form-row">
+      <div class="form-group col-md-3">
+        <label for="name">Year</label>
+        <input type="text" name="year" placeholder="Year" class="form-control">
+      </div>
+      <div class="form-group col-md-3">
+        <label for="name">Runtime</label>
+        <input type="text" name="name" placeholder="Runtime" class="form-control">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="image">Image</label>
+        <input type="file" name="image" class="form-control-file">
+      </div>
     </div>
-    <div class="form-group">
-      <label for="name">Year</label>
-      <input type="text" name="year" placeholder="Year" class="form-control">
-    </div>
-    <div class="form-group">
-      <label for="name">Runtime</label>
-      <input type="text" name="name" placeholder="Runtime" class="form-control">
-    </div>
-    <div class="form-group">
-      <label for="name">Image</label>
-      <input type="text" name="name" placeholder="png or jpg/jpeg" class="form-control">
-    </div>
-    <div class="form-group">
-      <input type="button" value="Add movie" class="btn btn-primary">
+    <div class="form-row">
+      <div class="form-group col-md-12">
+        <input type="button" value="Add movie" class="btn btn-primary">
+      </div>
     </div>
   </form>
 @endsection
